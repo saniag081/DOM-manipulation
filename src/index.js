5160a2754f841452517c13b9f8a4657317bacf55
saniag081 <sgonzalezhernandez081@gmail.com>
2021-02-02 19:47:26 -0500

    create nodos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+(function() {
+	console.log('Happy hacking :)')
+	const url = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+	async function fetchData() {
+		const request = await fetch(url)
+		const data = await request.json()
+		return data
+	}
+
+	function createElementHtml(etiqueta) {
+		return document.createElement(etiqueta)
+	}
+
+	function eachElement(arr) {
+		const allTheNodos = []
+		arr.forEach(element => {
+			const image = createElementHtml('img')
+			const title = createElementHtml('h2')
+			const price = createElementHtml('div')
+			const container = createElementHtml('div')
+			container.append(image, title, price)
+			allTheNodos.push(container)
+		});
+		document.body.append(...allTheNodos)
+	}
+
+	async function initial() {
+		const dataRequest = await fetchData()
+		eachElement(dataRequest.data)
+	}
+
+	initial()
+})()
