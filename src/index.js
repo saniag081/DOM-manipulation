cda2d8d5494fda455bfe150b059a81d09f01fc91
saniag081 <sgonzalezhernandez081@gmail.com>
2021-02-04 22:02:20 -0500

    add data to nodos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 (function() {
-	console.log('Happy hacking :)')
-	const url = "https://platzi-avo.vercel.app/api/avo";
+	const url = "https://platzi-avo.vercel.app";
+	const appNode = document.querySelector('#app')
 
 	async function fetchData() {
-		const request = await fetch(url)
+		const request = await fetch(`${url}/api/avo`)
 		const data = await request.json()
 		return data
 	}
 
 	function createElementHtml(etiqueta) {
 		return document.createElement(etiqueta)
+	}
+
+	function addTheTextToElements(data) {
+		data.forEach(detail => {
+			detail.src
+				? detail.elem.src = detail.src
+				: detail.elem.textContent = detail.value
+		} )
 	}
 
 	function eachElement(arr) {
@@ -19,10 +27,15 @@
 			const title = createElementHtml('h2')
 			const price = createElementHtml('div')
 			const container = createElementHtml('div')
+			addTheTextToElements([
+				{ elem: image, src: `${url}${element.image}` },
+				{ elem: title, value: element.name },
+				{ elem: price, value: element.price}
+			])
 			container.append(image, title, price)
 			allTheNodos.push(container)
 		});
-		document.body.append(...allTheNodos)
+		appNode.append(...allTheNodos)
 	}
 
 	async function initial() {
